2d23a58edcdec05cf4ec32f39dfbfda8ab31f567
Thomas-George-T <tgt555@gmail.com>
2023-01-19 14:51:39 -0500

    Replaced plotly chart with e-charts

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 Function to import Transformations and run the streamlit dashboard
 """
 import time
+import json
 import altair as alt
-import plotly.graph_objects as go
 import streamlit as st
+from streamlit_echarts import st_echarts
 from millify import millify
 from transform import parse_video
 from yt_metrics import youtube_metrics
+
 
 st.set_page_config(
     page_title="YouTube Analytics Dashboard",
@@ -17,13 +19,14 @@ st.set_page_config(
 
 st.title('YouTube Analytics Dashboard')
 
-video_url = st.text_input('Enter URL', placeholder='Example: https://www.youtube.com/watch?v=Il0S8BoucSA')
+video_url = st.text_input('Enter URL',
+                          placeholder='Example: https://www.youtube.com/watch?v=Il0S8BoucSA')
 if video_url:
-    with st.spinner('Crunching numbers...'):
-        time.sleep(5)
-
     df = parse_video(video_url)
     df_metrics = youtube_metrics(video_url)
+
+    with st.spinner('Crunching numbers...'):
+        time.sleep(3)
 
     # Metrics
     col1, col2, col3 = st.columns(3)
@@ -38,7 +41,7 @@ if video_url:
     st.subheader("Most liked comments")
     df_top = df[['Author', 'Comment', 'Timestamp', 'Likes']].sort_values(
         'Likes', ascending=False).reset_index(drop=True)
-    st.table(df_top.head())
+    st.table(df_top.head(10))
 
     # Top Languages
     st.subheader("Languages")
@@ -52,29 +55,54 @@ if video_url:
 
     st.altair_chart(lang, theme='streamlit', use_container_width=True)
 
-    # Sentiments of the Commentors
-    st.subheader("General Sentiment")
-    sentiments = df[df['Language'] == 'English']
-    data_sentiments = sentiments['TextBlob_Sentiment_Type'].value_counts(
-    ).rename_axis('Sentiment').reset_index(name='counts')
-
-    palette = {"POSITIVE": "#3E6D9C",
-               "NEGATIVE": "#EB455F",
-               "NEUTRAL": "#FCFFE7"}
-    fig_sentiment = go.Figure(
-        go.Pie(values=data_sentiments['counts'],
-               labels=data_sentiments['Sentiment'],
-               hole=.3,
-               texttemplate="%{label}<br>(%{percent})",
-               textposition="outside",
-               showlegend=False,
-               marker_colors=data_sentiments["Sentiment"].map(palette)
-               )
-    )
-    st.plotly_chart(fig_sentiment, use_container_width=True, theme='streamlit')
 
     # Most Replied Comments
     st.subheader("Most Replied Comments")
     df_replies = df[['Author', 'Comment', 'Timestamp', 'TotalReplies']].sort_values(
         'TotalReplies', ascending=False).reset_index(drop=True)
     st.table(df_replies.head())
+
+    # Sentiments of the Commentors
+    sentiments = df[df['Language'] == 'English']
+    data_sentiments = sentiments['TextBlob_Sentiment_Type'].value_counts(
+    ).rename_axis('Sentiment').reset_index(name='counts')
+
+    result = data_sentiments.to_json(orient="split")
+    parsed = json.loads(result)
+
+    options = {
+        "tooltip": {"trigger": "item"},
+        "legend": {"top": "5%", "left": "center"},
+        "series": [
+            {
+                "name": "Sentiment",
+                "type": "pie",
+                "radius": ["40%", "70%"],
+                "avoidLabelOverlap": False,
+                "itemStyle": {
+                    "borderRadius": 10,
+                    "borderColor": "#fff",
+                    "borderWidth": 2,
+                },
+                "label": {"show": False, "position": "center"},
+                "emphasis": {
+                    "label": {"show": True, "fontSize": "30", "fontWeight": "bold"}
+                },
+                "labelLine": {"show": False},
+                "data": [
+                    # NEUTRAL
+                    {"value": parsed['data'][1][1],
+                      "name": parsed['data'][1][0]},
+                    # POSITIVE
+                    {"value": parsed['data'][0][1],
+                      "name": parsed['data'][0][0]},
+                    # NEGATIVE
+                    {"value": parsed['data'][2][1],
+                      "name": parsed['data'][2][0]}
+                ],
+            }
+        ],
+    }
+    st_echarts(
+        options=options, height="500px",
+    )
