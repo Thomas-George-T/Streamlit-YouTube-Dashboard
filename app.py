ef398989b6b438c243cbb7b8b8e0aa271f1c55d3
Thomas-George-T <tgt555@gmail.com>
2023-01-19 16:44:27 -0500

    Refactoring

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 """
 Function to import Transformations and run the streamlit dashboard
 """
-import time
 import json
 import streamlit as st
 from streamlit_echarts import st_echarts
 from millify import millify
-from transform import parse_video
-from yt_metrics import youtube_metrics
+from transform import parse_video, youtube_metrics
 
 
 st.set_page_config(
@@ -18,99 +16,98 @@ st.set_page_config(
 
 st.title('YouTube Analytics Dashboard')
 
-video_url = st.text_input('Enter URL')
+VIDEO_URL = st.text_input('Enter URL')
 
-if st.button('Sample URL'):
-    st.text('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+if st.button('Try Example'):
+    VIDEO_URL='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 
 try:
-    if video_url:
-        df = parse_video(video_url)
-        df_metrics = youtube_metrics(video_url)
-
+    if VIDEO_URL:
         with st.spinner('Crunching numbers...'):
-            time.sleep(3)
+            df = parse_video(VIDEO_URL)
+            df_metrics = youtube_metrics(VIDEO_URL)
 
-        # Metrics
-        col1, col2, col3 = st.columns(3)
-        col1.metric("**Views**", millify(df_metrics[0], precision=2))
-        col2.metric("**Likes**", millify(df_metrics[1], precision=2))
-        col3.metric("**Comments**", millify(df_metrics[2], precision=2))
+            # Metrics
+            col1, col2, col3 = st.columns(3)
+            col1.metric("**Views**", millify(df_metrics[0], precision=2))
+            col2.metric("**Likes**", millify(df_metrics[1], precision=2))
+            col3.metric("**Comments**", millify(df_metrics[2], precision=2))
 
-        # Embedded Video
-        st.video(video_url)
+            # Embedded Video
+            st.video(VIDEO_URL)
 
-        # Top Comments
-        st.subheader("Most liked comments")
-        df_top = df[['Author', 'Comment', 'Timestamp', 'Likes']].sort_values(
-            'Likes', ascending=False).reset_index(drop=True)
-        st.table(df_top.head(11))
+            # Top Comments
+            st.subheader("Most liked comments")
+            df_top = df[['Author', 'Comment', 'Timestamp', 'Likes']].sort_values(
+                'Likes', ascending=False).reset_index(drop=True)
+            st.table(df_top.head(11))
 
-        # Top Languages
-        st.subheader("Languages")
-        df_langs = df['Language'].value_counts().rename_axis(
-            'Language').reset_index(name='count')
+            # Top Languages
+            st.subheader("Languages")
+            df_langs = df['Language'].value_counts().rename_axis(
+                'Language').reset_index(name='count')
 
-        options2 = {
-            "yAxis": {
-                "type": "category",
-                "data": df_langs['Language'].tolist(),
-            },
-            "xAxis": {"type": "value"},
-            "series": [{"data": df_langs['count'].tolist(), "type": "bar"}],
-        }
-        st_echarts(options=options2, height="500px")
+            options2 = {
+                "yAxis": {
+                    "type": "category",
+                    "data": df_langs['Language'].tolist(),
+                },
+                "xAxis": {"type": "value"},
+                "series": [{"data": df_langs['count'].tolist(), "type": "bar"}],
+            }
+            st_echarts(options=options2, height="500px")
 
-        # Most Replied Comments
-        st.subheader("Most Replied Comments")
-        df_replies = df[['Author', 'Comment', 'Timestamp', 'TotalReplies']].sort_values(
-            'TotalReplies', ascending=False).reset_index(drop=True)
-        st.table(df_replies.head(11))
+            # Most Replied Comments
+            st.subheader("Most Replied Comments")
+            df_replies = df[['Author', 'Comment', 'Timestamp', 'TotalReplies']].sort_values(
+                'TotalReplies', ascending=False).reset_index(drop=True)
+            st.table(df_replies.head(11))
 
-        # Sentiments of the Commentors
-        st.subheader("Sentiments")
-        sentiments = df[df['Language'] == 'English']
-        data_sentiments = sentiments['TextBlob_Sentiment_Type'].value_counts(
-        ).rename_axis('Sentiment').reset_index(name='counts')
+            # Sentiments of the Commentors
+            st.subheader("Sentiments")
+            sentiments = df[df['Language'] == 'English']
+            data_sentiments = sentiments['TextBlob_Sentiment_Type'].value_counts(
+            ).rename_axis('Sentiment').reset_index(name='counts')
 
-        result = data_sentiments.to_json(orient="split")
-        parsed = json.loads(result)
+            result = data_sentiments.to_json(orient="split")
+            parsed = json.loads(result)
 
-        options = {
-            "tooltip": {"trigger": "item"},
-            "legend": {"top": "5%", "left": "center"},
-            "series": [
-                {
-                    "name": "Sentiment",
-                    "type": "pie",
-                    "radius": ["40%", "70%"],
-                    "avoidLabelOverlap": False,
-                    "itemStyle": {
-                        "borderRadius": 10,
-                        "borderColor": "#fff",
-                        "borderWidth": 2,
-                    },
-                    "label": {"show": False, "position": "center"},
-                    "emphasis": {
-                        "label": {"show": True, "fontSize": "30", "fontWeight": "bold"}
-                    },
-                    "labelLine": {"show": False},
-                    "data": [
-                        # NEUTRAL
-                        {"value": parsed['data'][1][1],
-                        "name": parsed['data'][1][0]},
-                        # POSITIVE
-                        {"value": parsed['data'][0][1],
-                        "name": parsed['data'][0][0]},
-                        # NEGATIVE
-                        {"value": parsed['data'][2][1],
-                        "name": parsed['data'][2][0]}
-                    ],
-                }
-            ],
-        }
-        st_echarts(
-            options=options, height="500px",
-        )
+            options = {
+                "tooltip": {"trigger": "item"},
+                "legend": {"top": "5%", "left": "center"},
+                "series": [
+                    {
+                        "name": "Sentiment",
+                        "type": "pie",
+                        "radius": ["40%", "70%"],
+                        "avoidLabelOverlap": False,
+                        "itemStyle": {
+                            "borderRadius": 10,
+                            "borderColor": "#fff",
+                            "borderWidth": 2,
+                        },
+                        "label": {"show": False, "position": "center"},
+                        "emphasis": {
+                            "label": {"show": True, "fontSize": "30", "fontWeight": "bold"}
+                        },
+                        "labelLine": {"show": False},
+                        "data": [
+                            # NEUTRAL
+                            {"value": parsed['data'][1][1],
+                             "name": parsed['data'][1][0]},
+                            # POSITIVE
+                            {"value": parsed['data'][0][1],
+                             "name": parsed['data'][0][0]},
+                            # NEGATIVE
+                            {"value": parsed['data'][2][1],
+                             "name": parsed['data'][2][0]}
+                        ],
+                    }
+                ],
+            }
+            st_echarts(
+                options=options, height="500px",
+            )
 except:
-    st.error(' The URL Should be of the form: https://www.youtube.com/watch?v=videoID', icon="🚨")
+    st.error(
+        ' The URL Should be of the form: https://www.youtube.com/watch?v=videoID', icon="🚨")
--- transform.py
+++ transform.py
@@ -132,8 +132,43 @@ def parse_video(url) -> pd.DataFrame:
 
     return df_transform
 
+def youtube_metrics(url) -> list:
+    """ Function to get views, likes and comment counts
+    Args:
+        URL: url of the youtube video
+    Returns:
+        List containing views, likes and comment counts
+    """
+
+    # Get the video_id from the url
+    video_id = url.split('?v=')[-1]
+
+    # creating youtube resource object
+    youtube = googleapiclient.discovery.build(
+        'youtube', 'v3', developerKey=st.secrets["api_key"])
+
+    statistics_request = youtube.videos().list(
+        part="statistics",
+        id=video_id
+    ).execute()
+
+    metrics = []
+
+    # extracting required info from each result object
+    for item in statistics_request['items']:
+
+        # Extracting views
+        metrics.append(item['statistics']['viewCount'])
+        # Extracting likes
+        metrics.append(item['statistics']['likeCount'])
+        # Extracting Comments
+        metrics.append(item['statistics']['commentCount'])
+
+    return metrics
+
 
 if __name__ == "__main__":
     df_main = parse_video('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    df_yt = youtube_metrics('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
     print(df_main.head())
-    print(df_main['Timestamp'].head())
+    print(df_yt)
